f9909ead9efa4c3e33d800fd26ee82975f33c3af
sabarish244 <sabarishsabarish244@gmail.com>
2026-04-17 12:41:14 +0530

    Update AWS Secrets module version and add replica regions variable

--- variables.tf
+++ variables.tf
@@ -4,6 +4,15 @@ variable "admin_principals" {
   default     = null
 }
 
+variable "replica_regions" {
+  description = "List of regions to replicate the secret to"
+  type = list(object({
+    region     = string
+    kms_key_id = optional(string)
+  }))
+  default = []
+}
+
 variable "domain" {
   type        = string
   description = "The domain from which emails are sent"
